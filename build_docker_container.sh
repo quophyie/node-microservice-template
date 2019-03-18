65de798e655115e481d6ebd68cd2900a00f521ba
quophyie <quophyie@yahoo.com>
2019-03-18 23:59:21 +0000

    Updated docker-compose yml with defaults for env vars WEB_APP_PORT and DB_PORT_EXPOSED_ON_HOST
    Added
     - script/build_run_docker_container.sh
     - script/variables.sh
    
     Deleted build_docker_container.sh

--- build_docker_container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-. ~/.bash_profile
-docker-compose -f docker/compose/docker-compose.yml build --no-cache  && docker-compose -f docker/compose/docker-compose.yml up
---
+++ scripts/variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#The db container name
+DB_CONTAINER_NAME=node_micrososervice_db
+
+# the db server port that is exposed to the host / outside world
+DB_PORT_EXPOSED_ON_HOST=5435
+
+# the db server port that is exposed on the container network i.e. (the default host:port on the container side)
+DB_PORT_EXPOSED_ON_DB_CONTAINER=5432
+
+# the web app server port that is exposed to the host / outside world
+WEB_APP_PORT=4000
+
+# for node apps only
+NODE_ENV=development
