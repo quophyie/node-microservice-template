6b9864cd686996891510b8b91c54581a47b719aa
quophyie <quophyie@yahoo.com>
2019-03-21 14:48:11 +0000

    Updated  - wait-for-it.sh timeout in docker-compose.yml to 30 secs  - updated scripts/variables.sh with the MS_NAME variable i.e. microservice name
    Added
     - build_run_docker_microservice_containers.sh
    
    Deleted
     - scripts/build_run_docker_container.sh

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -12,5 +12,7 @@ DB_PORT_EXPOSED_ON_DB_CONTAINER=5432
 # the web app server port that is exposed to the host / outside world
 WEB_APP_PORT=4000
 
+MS_NAME=$(echo $(pwd) | rev | cut -d'/' -f 2 | rev)
+
 # for node apps only
 NODE_ENV=development
